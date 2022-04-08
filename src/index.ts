f7c87bb582a78738ffe0a87b70a9fb4829abded7
Lucky Soni <lp.soni@yahoo.co.in>
2022-04-09 00:25:50 +0530

    Add Prettier config and reformat

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // Validate that a switch/case is exhaustive
 export function exhaustive(x: never, ty?: string): never {
-  const prefix =
-    ty === undefined || ty === null
-      ? "Unrecognized tag"
-      : `Unrecognized tag for ${ty}`;
-  throw new Error(`${prefix}: ${saferStringify(x)}`);
+  const prefix = ty === undefined || ty === null ? 'Unrecognized tag' : `Unrecognized tag for ${ty}`
+  throw new Error(`${prefix}: ${saferStringify(x)}`)
 }
 
 // Validate that a reducer switch/case is exhaustive
@@ -12,20 +9,20 @@ export function exhaustive(x: never, ty?: string): never {
 // In the failing case the provided state will be returned
 //
 export function exhaustiveReducer<T>(_x: never, state: T): T {
-  return state;
+  return state
 }
 
 // JSON.stringify ignores undefined and throws on circular objects
 function saferStringify(root: unknown): string {
   if (root === undefined) {
-    return "undefined";
+    return 'undefined'
   }
   try {
-    return JSON.stringify(root);
+    return JSON.stringify(root)
   } catch (e) {
     if (e instanceof TypeError) {
-      return "{...Circular object or BigInt...}";
+      return '{...Circular object or BigInt...}'
     }
-    throw e;
+    throw e
   }
 }
