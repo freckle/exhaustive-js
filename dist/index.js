5213976eb0759792b83be375f0ccd66bce7cbc05
Restyled.io <commits@restyled.io>
2022-04-08 19:00:45 +0000

    Restyled by prettier

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value : true});
-exports.exhaustiveReducer = exports.exhaustive = void 0;
+'use strict'
+Object.defineProperty(exports, '__esModule', {value: true})
+exports.exhaustiveReducer = exports.exhaustive = void 0
 // Validate that a switch/case is exhaustive
 function exhaustive(x, ty) {
-  const prefix = ty === undefined || ty === null ? "Unrecognized tag"
-                                                 : `Unrecognized tag for ${ty}`;
-  throw new Error(`${prefix}: ${saferStringify(x)}`);
+  const prefix = ty === undefined || ty === null ? 'Unrecognized tag' : `Unrecognized tag for ${ty}`
+  throw new Error(`${prefix}: ${saferStringify(x)}`)
 }
-exports.exhaustive = exhaustive;
+exports.exhaustive = exhaustive
 // Validate that a reducer switch/case is exhaustive
 //
 // In the failing case the provided state will be returned
 //
-function exhaustiveReducer(_x, state) { return state; }
-exports.exhaustiveReducer = exhaustiveReducer;
+function exhaustiveReducer(_x, state) {
+  return state
+}
+exports.exhaustiveReducer = exhaustiveReducer
 // JSON.stringify ignores undefined and throws on circular objects
 function saferStringify(root) {
   if (root === undefined) {
-    return "undefined";
+    return 'undefined'
   }
   try {
-    return JSON.stringify(root);
+    return JSON.stringify(root)
   } catch (e) {
     if (e instanceof TypeError) {
-      return "{...Circular object or BigInt...}";
+      return '{...Circular object or BigInt...}'
     }
-    throw e;
+    throw e
   }
 }
